0151d67057d4b36717b468cff6b782d7b0f74932
HanChuenFoong <hanchuen.foong@gmail.com>
2022-11-22 01:47:30 -0800

    Update dependecies

--- server/server.js
+++ server/server.js
@@ -1,17 +1,49 @@
-require('dotenv').config()
+require('dotenv').config({path: "./.env"})
 
 const express = require('express')
 const app = express()
 app.use(express.json())
+const cors = require("cors")
+app.use(cors({
+  origin: "http://localhost:8080"
+}))
 
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY)
 
-const storeItems = new Map([
+const USER_SHOPPING_CART = [
+  {
+    id: "prod_MqZ1SmAp1VN8aC",
+    default_price: "price_1M6rtgAnsxsStcQP8rwkbIEI",
+    quantity: 1,
+  },
+  {
+    id: "prod_MqZ7WfUEU6YX8j",
+    default_price: "price_1M6rziAnsxsStcQPwISlocfS",
+    quantity: 1,
+  },
+  {
+    id: "prod_MqZ9e6zxgCS0cs",
+    default_price: "price_1M6s13AnsxsStcQPrcSwUuwU",
+    quantity: 1,
+  }
+]
 
-])
 // Get backend to load here
-app.post("create-checkout-session", async (req, res) => {
-  res.json({url: "Hi"})
+app.post("/create-checkout-session", async (req, res) => {
+  const lineItems = USER_SHOPPING_CART.map((item) => {
+    return {
+      price: item.default_price,
+      quantity: item.quantity,
+    };
+  });
+  console.log("Working")
+  const session = await stripe.checkout.sessions.create({
+    mode: "payment",
+    line_items: lineItems,
+    success_url: `http://localhost:8080/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `http://localhost:8080/declined`,
+  });
+  return res.send({ url: session.url });
 })
 
 app.listen(3000)
